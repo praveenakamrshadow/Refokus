0b734c1e7cab0b6c2e4d3606ffbbcc5e1728990b
praveen Sahu <praveen.pkyt327867@gmail.com>
2024-01-04 22:11:42 +0530

    hero component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import NavBar from './components/NavBar';
-
+import Work from './components/Work';
 function App() {
     return (
         <h1 className='w-full h-screen bg-zinc-900 font-["Satoshi Variable"]  text-white'>
             <NavBar />
+            <Work />
         </h1>
     );
 }
